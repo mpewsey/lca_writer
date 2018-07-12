575b903e742293c362fac673279c525b38db8daf
Matt Pewsey <mattpewsey@gmail.com>
2018-07-12 18:59:35 -0400

    Update python_required

--- setup.py
+++ setup.py
@@ -17,4 +17,4 @@ setup(name = 'lca_writer',
       dependency_links = [],
       keywords = [],
       classifiers = [],
-      python_requires = '')
+      python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*')
