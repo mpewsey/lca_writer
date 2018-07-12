b7e313d349094f3c64679c8cb63e6258011142bf
Matt Pewsey <mattpewsey@gmail.com>
2018-07-12 19:14:12 -0400

    Add docs

---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+LCA Writer
+==========
+
+.. automodule:: lca_writer.lca_writer
+   :members:
+
+
+.. automodule:: lca_writer.data.loader
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
--- lca_writer/data/loader.py
+++ lca_writer/data/loader.py
@@ -4,5 +4,12 @@ from ..lca_writer import LCAWriter
 DATA_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 def load_data(name):
+    """
+    Loads an Excel form from the data folder with the specified name.
+
+    Parameters:
+        name : str
+            The name of the form without file extension.
+    """
     p = os.path.join(DATA_FOLDER, name + '.xlsx')
     return LCAWriter(p)
